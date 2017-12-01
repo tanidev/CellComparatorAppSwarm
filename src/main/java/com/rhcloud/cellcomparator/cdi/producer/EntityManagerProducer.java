57013926788dbbc2a10f554af0aa40ea68993c65
estani223225 <estani223225@W001INF013.br-atacadao.corp>
2017-12-01 10:11:00 -0200

    Limpeza de imports

--- src/main/java/com/rhcloud/cellcomparator/cdi/producer/EntityManagerProducer.java
+++ src/main/java/com/rhcloud/cellcomparator/cdi/producer/EntityManagerProducer.java
@@ -4,7 +4,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
-import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
